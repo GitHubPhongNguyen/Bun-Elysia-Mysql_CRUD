335dfeb2bac2744dfba80a36fd735bc65d683d77
phongnv3 <phongnv3@rikkeisoft.com>
2024-01-31 08:22:27 +0700

    init: Bun + Elysia + mySql CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,51 @@
+import swagger from "@elysiajs/swagger";
+import { PrismaClient } from "@prisma/client";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+interface Post {
+  id?: number
+  title: string
+  path: string
+  content: string
+}
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const prisma = new PrismaClient({
+  log: ['info', 'warn', 'error']
+})
+
+const app = new Elysia().decorate('db', prisma).use(swagger()) 
+
+app.get('/posts', ({db}) => {
+  return db.post.findMany()
+})
+
+app.get('/posts/:id', ({db, params}) => {
+  return db.post.findUnique({ where: { id: Number(params.id) } })
+})
+
+app.get('/posts/path/:path', ({db, params}) => {
+  return db.post.findUnique({ where: { path: String(params.path) } })
+})
+
+app.post('/post', ({db, body}) => {
+  return db.post.create({
+    data: body as Post
+  })
+})
+
+app.put('/post/:id', ({db, params, body}) => {
+  return db.post.update({
+    where: { id: Number(params.id)},
+    data: body as Post
+  })
+})
+
+app.delete('/post/:id', ({db, params}) => {
+  return db.post.delete({
+    where: { id: Number(params.id)}
+  })
+})
+
+app.listen(process.env.API_PORT || 3000, () => {
+  console.log(`🚀 ~ app.listen on port: ${app.server?.port}`)
+})
